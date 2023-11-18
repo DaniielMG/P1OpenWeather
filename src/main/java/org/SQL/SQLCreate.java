c024a077a693680fbc30339cdbd377b272cd3d00
danie <danielmg012@gmail.com>
2023-11-18 19:06:01 +0000

    WE HAVE CORRECTED SOME SMALL ERRORS THAT DID NOT ALLOW THE DATABASE TO BE UPDATED

--- src/main/java/org/SQL/SQLCreate.java
+++ src/main/java/org/SQL/SQLCreate.java
@@ -1,20 +1,13 @@
 package org.SQL;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-import static org.Control.WeatherMapProvider.isProximaSemana;
+import static org.SQL.SQLInsert.processWeatherResponse;
 
 public class SQLCreate {
 	public static void fetchWeatherData(double lat, double lon, String locationName) {
@@ -25,7 +18,6 @@ public class SQLCreate {
 		String tableName = "WeatherData_" + locationName.replaceAll("\\s+", "_").toLowerCase();
 
 		try (Connection conn = connect.connect(databasePath)) {
-			// Crear tabla para la ubicación específica
 			try (Statement statement = conn.createStatement()) {
 				String createTableQuery = String.format(
 						"CREATE TABLE IF NOT EXISTS %s (" +
@@ -39,7 +31,6 @@ public class SQLCreate {
 				statement.execute(createTableQuery);
 			}
 
-			// Obtener datos del clima y almacenarlos en la base de datos
 			URL url = new URL(apiUrl);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
@@ -59,42 +50,6 @@ public class SQLCreate {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-	private static void processWeatherResponse(String jsonResponse, String tableName, Connection conn) throws SQLException {
-		JSONObject json = new JSONObject(jsonResponse);
-		JSONArray forecastList = json.getJSONArray("list");
-		JSONObject cityList = json.getJSONObject("city");
-		LocalDateTime currentDate = LocalDateTime.now();
-		String insertQuery = String.format("INSERT INTO %s (forecastDateTime, temperature, description, nubosity, windSpeed) VALUES (?, ?, ?, ?, ?)", tableName);
-
-		for (int i = 0; i < forecastList.length(); i++) {
-			JSONObject forecast = forecastList.getJSONObject(i);
-			String timestamp = forecast.getString("dt_txt");
-			LocalDateTime forecastDateTime = LocalDateTime.parse(timestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-			if (isProximaSemana(forecastDateTime, currentDate) && forecastDateTime.getHour() == 12) {
-				JSONObject main = forecast.getJSONObject("main");
-				double temperature = main.getDouble("temp");
-				JSONArray weatherArray = forecast.getJSONArray("weather");
-				JSONObject weather = weatherArray.getJSONObject(0);
-				String description = weather.getString("description");
-				JSONObject clouds = forecast.getJSONObject("clouds");
-				int nubosity = clouds.getInt("all");
-				JSONObject wind = forecast.getJSONObject("wind");
-				double windSpeed = wind.getDouble("speed");
-
-				// Insertar datos en la tabla
-				try (PreparedStatement pstmt = conn.prepareStatement(insertQuery)) {
-					pstmt.setString(1, timestamp);
-					pstmt.setDouble(2, temperature);
-					pstmt.setString(3, description);
-					pstmt.setDouble(4, nubosity);
-					pstmt.setDouble(5, windSpeed);
-					pstmt.executeUpdate();
-				}
-			}
 		}
 	}
 }
--- src/main/java/org/SQL/SQLInsert.java
+++ src/main/java/org/SQL/SQLInsert.java
@@ -1,26 +1,49 @@
 package org.SQL;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.Control.WeatherMapProvider.isProximaSemana;
 
 public class SQLInsert {
-	public static void insert(String municipality, String forecastDateTime,double temperature, String description, double nubosity, double windSpeed) {
-		String sql = "INSERT INTO WeatherData (municipality,forecastDateTime, temperature, description, nubosity, windSpeed) " +
-				"VALUES (?, ?, ?, ?, ?, ?)";
-		String dbPath = "src\\main\\resources\\DataBase.db";
-		SQLConnect connect = new SQLConnect();
-		try (Connection conn = connect.connect(dbPath);
-			 PreparedStatement pstmt =  conn.prepareStatement(sql)) {
-			pstmt.setString(1, municipality);
-			pstmt.setString(2, forecastDateTime);
-			pstmt.setDouble(3, temperature);
-			pstmt.setString(4, description);
-			pstmt.setDouble(5, nubosity);
-			pstmt.setDouble(6, windSpeed);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+	static void processWeatherResponse(String jsonResponse, String tableName, Connection conn) throws SQLException {
+		JSONObject json = new JSONObject(jsonResponse);
+		JSONArray forecastList = json.getJSONArray("list");
+		JSONObject cityList = json.getJSONObject("city");
+		LocalDateTime currentDate = LocalDateTime.now();
+		String insertQuery = String.format("INSERT INTO %s (forecastDateTime, temperature, description, nubosity, windSpeed) VALUES (?, ?, ?, ?, ?)", tableName);
+
+		for (int i = 0; i < forecastList.length(); i++) {
+			JSONObject forecast = forecastList.getJSONObject(i);
+			String timestamp = forecast.getString("dt_txt");
+			LocalDateTime forecastDateTime = LocalDateTime.parse(timestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+			if (isProximaSemana(forecastDateTime, currentDate) && forecastDateTime.getHour() == 12) {
+				JSONObject main = forecast.getJSONObject("main");
+				double temperature = main.getDouble("temp");
+				JSONArray weatherArray = forecast.getJSONArray("weather");
+				JSONObject weather = weatherArray.getJSONObject(0);
+				String description = weather.getString("description");
+				JSONObject clouds = forecast.getJSONObject("clouds");
+				int nubosity = clouds.getInt("all");
+				JSONObject wind = forecast.getJSONObject("wind");
+				double windSpeed = wind.getDouble("speed");
+
+				try (PreparedStatement pstmt = conn.prepareStatement(insertQuery)) {
+					pstmt.setString(1, timestamp);
+					pstmt.setDouble(2, temperature);
+					pstmt.setString(3, description);
+					pstmt.setDouble(4, nubosity);
+					pstmt.setDouble(5, windSpeed);
+					pstmt.executeUpdate();
+				}
+			}
 		}
 	}
 }
